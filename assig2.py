efbdc91391ad74f8ae594ada832d1da85390249d
BuildTools <unconfigured@null.spigotmc.org>
2021-04-09 10:30:33 +0200

    Extra functions & assignment 2

---
+++ assig2.py
@@ -0,0 +1,119 @@
+import pywikibot as pw
+from pywikibot import pagegenerators
+import wikipeoplelib as wlib
+from wikipeoplelib import enwiki, commons
+import csv
+import time
+import mwparserfromhell as mw
+
+folder = "testfolder/"
+imgfolder = folder+"imgs/"
+
+"""
+all pages
+1. image
+2. sex
+3. date of birth
+4. image inception
+5. image description
+6. date of death
+7. first sentence
+get info from multiple sources where possible
+"""
+
+def timeGen(gen, maxpages, catname,exclude=None):
+    print("Starting processing of at most",maxpages,"pages from",catname)
+    start = time.time()
+    total = 0
+    images = 0
+    excluded = 0
+    with open(folder+catname+'.csv', "w", newline='', encoding="utf-8") as cf:
+        writer = csv.writer(cf)
+        writer.writerow(["Name","Sex","Birthdate","Deathdate","ImageName","ImageDescription", "ImageDate"])
+        for page in gen:
+            if exclude:
+                if exclude in page.title():
+                    excluded += 1
+                    continue
+            total += 1
+            if wlib.hasPageImage(page):
+                images += 1
+                img = wlib.getPageImage(page)
+                infotemp = wlib.getInfoTemplate(img)
+                desc = wlib.getDescription(infotemp)
+                date = wlib.getDate(infotemp)
+                #print("-----------------\n",page.title())
+                desc = "NoDesc" if desc is None else str(desc).replace("\n","\\n")
+                date = "NoDate" if date is None else str(date).replace("\n","\\n")
+                item = page.data_item()
+                item.get()
+                sex = wlib.getSex(item)
+                sex = "NoSex" if not sex else sex
+                birthdate = wlib.getBirthDate(item)
+                birthdate = "NoBirthDate" if not birthdate else birthdate
+                deathdate = wlib.getDeathDate(item)
+                deathdate = "NoDeathDate" if not deathdate else deathdate
+                filename = img.title(as_filename=True, with_ns=False)
+                filenameWithPath = imgfolder+filename
+                #img.download(filename=filenameWithPath)
+                writer.writerow([page.title(),sex,birthdate,deathdate,filename,desc,date])
+                #break
+
+            if total % 100 == 0:
+                tm = time.time()
+                print(total, "/", maxpages,"pages processed")
+                print(tm-start,"s elapsed so far")
+            
+    end = time.time()
+    print("total", total)
+    print("images", images)
+    print("excluded", excluded)
+    print(100*images/total, "%")
+    print("Time:", end-start,"s")
+    return {"total":total,"images":images,"excluded":excluded,"%":100*images/total,"time":end-start}
+
+def livingppl():
+    prefix = 'Category:'
+    catname = 'Living people'
+    cat = pw.Category(enwiki,prefix+catname)
+    sampleSize = 1000
+    sortkey = "!ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    gen = wlib.uniformPageGenerator(cat, sampleSize, sortkey)
+    total = len(sortkey)*sampleSize
+    d = timeGen(gen, total,catname)
+    d['name'] = catname
+    return d
+
+def deaths(year):
+    prefix = 'Category:'
+    catname = str(year)+'_deaths'
+    cat = pw.Category(enwiki, prefix+catname)
+    print(cat, cat.categoryinfo)
+    sampleSize = 50
+    sortkey = "!ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    gen = wlib.uniformPageGenerator(cat, sampleSize, sortkey)
+    total = len(sortkey)*sampleSize
+    d = timeGen(gen, total,catname,exclude=str(year))
+    d['name'] = catname
+    return d
+    
+def processYears():
+    dicts = []
+    for i in range(2020,1999,-1):
+        dicts.append(deaths(i))
+    with open(folder+"info.csv", "w", encoding="utf-8",newline='') as cf:
+        writer = csv.writer(cf)
+        writer.writerow(["category","total_pages", "has_image", "time_seconds"])
+        for d in dicts:
+            writer.writerow([d["name"],d["total"],d["images"],d["time"]])
+
+if __name__ == "__main__":
+    livingppl()
+    """prefix = 'Category:'
+    catname = 'Living people'
+    cat = pw.Category(enwiki,prefix+catname)
+    gen = cat.articles(startprefix="D'Aniello")
+    sortkey = "!ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    print(timeGen(gen,1000,catname))"""
+    
+    #deaths(2020)
--- wikipeoplelib.py
+++ wikipeoplelib.py
@@ -1,4 +1,5 @@
 import pywikibot as pw
+import mwparserfromhell as mw
 
 enwiki = pw.Site("en", "wikipedia")
 commons = pw.Site("commons", "commons")
@@ -18,7 +19,10 @@ def getPageImage(page):
     :param page: Page object to be checked for page image
     :return: page image of specified page
     """
-    return page.page_image()
+    img = page.page_image()
+    if img.file_is_shared():
+        img = pw.FilePage(img.site.image_repository(), img.title())
+    return img
 
 def uniformPageGenerator(category, sampleSize = 10, sortkey = "!ABCDEFGHIJKLMNOPQRSTUVWXYZ"):
     """
@@ -33,3 +37,108 @@ def uniformPageGenerator(category, sampleSize = 10, sortkey = "!ABCDEFGHIJKLMNOP
         end = None if i+1 >= size else sortkey[i+1]
         gen = category.articles(total = sampleSize, startprefix=start, endprefix=end)
         yield from gen
+
+def getInfoTemplate(filepage):
+    """
+    Parse filepage text into templates (formatted text objects within curly braces)
+    and return the Information template, which contains image description, date and other info
+    :param filepage: existing filepage of specified image
+    :return: Parsed Information template with
+    """
+    templates = mw.parse(filepage.text).filter_templates()
+    infolist = [t for t in templates if "Information" in t]
+    if len(infolist) < 1:
+        return None
+    return infolist[0]
+
+def getDescription(infotemplate):
+    """
+    Retrieve the description attribute from image summary template
+    :param infotemplate: The Information template of said image
+    :return date: Contents of description attribute, None otherwise
+    """
+    if not infotemplate:
+        return None
+    if("Description" in infotemplate):
+        desc = infotemplate["Description"]
+    elif("description" in infotemplate):
+        desc = infotemplate["description"]
+    else:
+        desc = None
+    return desc
+
+def getDate(infotemplate):
+    """
+    Retrieve the date attribute from image summary template
+    :param infotemplate: The Information template of said image
+    :return date: Contents of date attribute, None otherwise
+    """
+    if not infotemplate:
+        return None
+    if("Date" in infotemplate):
+        date = infotemplate["Date"]
+    elif("date" in infotemplate):
+        date = infotemplate["date"]
+    else:
+        date = None
+    return date
+
+def getSex(item):
+    """
+    Retrieve persons sex from their wikidata itempage
+    :param item: ItemPage object of said person on wikidata
+    :return: sex in a string, None if no sex was found
+    """
+    
+    # get the persons wikidata page, the wikipedia page does not always contain the sex
+    sex = None
+    if item and item.claims:
+        # Property 21 (P21) is sex/gender
+        if "P21" in item.claims:
+            try:
+                target = item.claims["P21"][0].getTarget()
+                sex = target.text["labels"]["en"]
+            except Exception as e:
+                print(e)
+            except Error as e:
+                print(e)
+    return sex
+
+def getBirthDate(item):
+    """
+    Retrieve persons birth date from their wikidata itempage
+    :param item: ItemPage object of said person on wikidata
+    :return: YY-mm-dd birthdate string, None if no date was found
+    """
+    # get the persons wikidata page, the wikipedia page does not always contain the birthdate
+    birthdate = None
+    if item and item.claims:
+        # Property 569 (P569) is the birth_date
+        if "P569" in item.claims:
+            try:
+                target = item.claims["P569"][0].getTarget()
+                if(target):
+                    birthdate = target.toTimestamp().strftime("%Y-%m-%d")
+            except Exception as e:
+                print(e)
+    return birthdate
+
+def getDeathDate(item):
+    """
+    Retrieve persons deathdate from their wikidata itempage
+    :param item: ItemPage object of said person on wikidata
+    :return: YY-mm-dd deathdate string, None if no date was found
+    """
+    # get the persons wikidata page, the wikipedia page does not always contain the deathdate
+    
+    deathdate = None
+    if item and item.claims:
+        # Property 570 is the death_date
+        if "P570" in item.claims:
+            try:
+                target = item.claims["P570"][0].getTarget()
+                if(target):
+                    deathdate = target.toTimestamp().strftime("%Y-%m-%d")
+            except Exception as e:
+                print(e)
+    return deathdate
